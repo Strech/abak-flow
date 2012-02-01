3165d3be50480c169a68e4b4605a2ae06752a6f2
Strech (aka Sergey Fedorov) <strech_ftf@mail.ru>
2012-02-01 14:55:32 +0600

    Test

--- ghr.rb
+++ ghr.rb
@@ -1,26 +1,31 @@
+# -*- encoding: utf-8 -*-
+
 require 'rubygems'
 require 'commander/import'
+require 'hub'
 
 # :name is optional, otherwise uses the basename of this executable
-program :name, 'Foo Bar'
+program :name, 'Утилита для оформления pull request на github.com'
 program :version, '1.0.0'
-program :description, 'Stupid command that prints foo or bar.'
+program :description, 'Утилита, заточенная под git-flow но с использованием github.com'
 
-command :foo do |c|
-  c.syntax = 'foobar foo'
-  c.description = 'Displays foo'
+command :'request make' do |c|
+  c.syntax      = 'request make <Заголовок>'
+  c.description = 'Оформить pull request из текущей ветки (feature -> develop, hotfix -> master)'
+
+  c.option '--head STRING', String, 'Имя ветки, которую нужно принять в качестве изменений'
+  c.option '--base STRING', String, 'Имя ветки, в которую нужно принять изменения'
+
   c.action do |args, options|
-    say 'Fuuuuuuuuuu'
+    # 1. Проверить в какой мы ветке
+    # 2. Если не указано имя, берем имя ветки после слеша
+    # 3. Если
+
+    repo = Hub::Commands.send :local_repo
+    puts repo.current_branch
+
+    puts options.inspect
+    puts args.inspect
   end
 end
-
-command :bar do |c|
-  c.syntax = 'foobar bar [options]'
-  c.description = 'Display bar with optional prefix and suffix'
-  c.option '--prefix STRING', String, 'Adds a prefix to bar'
-  c.option '--suffix STRING', String, 'Adds a suffix to bar'
-  c.action do |args, options|
-    options.default :prefix => '(', :suffix => ')'
-    say "#{options.prefix}bar#{options.suffix}"
-  end
-end
+alias_command :request, :'request make'
