fa8e7a3a14a720f2bf5b2b42c51c104dfbb4a0de
Strech (aka Sergey Fedorov) <strech_ftf@mail.ru>
2013-01-24 00:38:43 +0600

    Branches spec refactoring

--- spec/abak-flow/branches_spec.rb
+++ spec/abak-flow/branches_spec.rb
@@ -6,9 +6,6 @@ describe Abak::Flow::Branches do
   let(:described_class) { Abak::Flow::Branches }
   
   describe "Interface" do
-    it "should respond to current_branch" do
-      described_class.must_respond_to :current_branch
-    end
+    it { described_class.must_respond_to :current_branch }
   end
-  
 end
