88122878e62fb89347edd9f054884bf358931ee0
Strech (aka Sergey Fedorov) <strech_ftf@mail.ru>
2013-01-24 00:41:53 +0600

    GithubClient spec refactoring

--- spec/abak-flow/github_client_spec.rb
+++ spec/abak-flow/github_client_spec.rb
@@ -5,8 +5,5 @@ require "abak-flow/github_client"
 describe Abak::Flow::GithubClient do
   let(:described_class) { Abak::Flow::GithubClient }
 
-  it "should respond to #connection" do
-    described_class.must_respond_to :connection
-  end
-
+  it { described_class.must_respond_to :connection }
 end
