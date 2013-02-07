91ba2590cf484b6108091c2e9dfc19aa6411d38a
Strech (aka Sergey Fedorov) <strech_ftf@mail.ru>
2013-02-08 00:31:23 +0600

    Messages

--- lib/abak-flow/messages.rb
+++ lib/abak-flow/messages.rb
@@ -1,25 +1,36 @@
+# coding : utf-8
+# TODO : Написать + реализовать
+require "i18n"
+
 module Abak::Flow
   class Messages
+    extend Forwardable
+
+    attr_reader :scope, :elements
 
     def initialize(scope)
+      init_dependences
+
+      @scope = scope
+      @elements = []
     end
 
     # Iterate elements from locale scope (translating online)
     #
     #
     def each
-    end
+      raise ArgumentError, "No block given" unless block_given?
 
-    # Print all elements from locale scope without header
-    #
-    # Returns Symbol
-    def to_s
+      elements.each do |key|
+        yield translate(key)
+      end
     end
 
     # Put item to elements
     #
     # Returns Symbol
     def push(element)
+      @elements << element.to_sym
     end
     alias :<< :push
 
@@ -27,6 +38,13 @@ module Abak::Flow
     #
     # Returns String
     def header
+      translate :header
+    end
+
+    # Print all elements from locale scope without header
+    #
+    # Returns Symbol
+    def to_s
     end
 
     # Print section header from locale scope and all elements from scope
@@ -36,5 +54,19 @@ module Abak::Flow
     end
     alias :pp :pretty_print
 
+    private
+    def_delegators Config, :locale
+
+    def init_dependences
+      Config.init
+    end
+
+    def translate(key)
+      I18n.t key, scope: scope_key
+    end
+
+    def scope_key
+      [locale, scope] * "."
+    end
   end
 end
--- spec/abak-flow/messages_spec.rb
+++ spec/abak-flow/messages_spec.rb
@@ -1,7 +1,17 @@
 # coding: utf-8
 require "spec_helper"
-require "abak-flow/messages"
 
+class Abak::Flow::Messages
+  module Config
+    def self.init; end
+
+    class << self
+      attr_accessor :locale
+    end
+  end
+end
+
+require "abak-flow/messages"
 describe Abak::Flow::Messages do
   describe "Interface" do
     subject { Abak::Flow::Messages.new "scope" }
@@ -13,5 +23,80 @@ describe Abak::Flow::Messages do
     it { subject.must_respond_to :header }
     it { subject.must_respond_to :pretty_print }
     it { subject.must_respond_to :pp }
+  end
+
+  describe "Inner methods" do
+    describe "#scope_key" do
+      subject { Abak::Flow::Messages.new "the_best_way" }
+
+      it "should return scope with locale" do
+        Abak::Flow::Messages::Config.stub(:locale, :ru) do
+          subject.send(:scope_key).must_equal "ru.the_best_way"
+        end
+      end
+    end
+  end
+
+  describe "Public methods" do
+    describe "#header" do
+      subject { Abak::Flow::Messages.new "pretty_header" }
+
+      it "should print +HEADER+" do
+        st = ->(k) { k == :header ? "+HEADER+" : nil }
+
+        subject.stub(:translate, st) do
+          subject.header.must_equal "+HEADER+"
+        end
+      end
+    end
+
+    describe "#push" do
+      subject { Abak::Flow::Messages.new "with_elements" }
+
+      it { subject.elements.must_equal [] }
+
+      it "should push one element" do
+        subject.push "hello"
+        subject.elements.must_equal [:hello]
+      end
+
+      it "should push two elements" do
+        subject.push "hello"
+        subject.push "world"
+        subject.elements.must_equal [:hello, :world]
+      end
+    end
+
+    describe "#each" do
+      subject { Abak::Flow::Messages.new "for_each" }
+
+      it { -> { subject.each }.must_raise ArgumentError }
+      it "should work with blocks and return array" do
+        tested = []
+        st = ->(k) do
+          case k
+          when :hello then "pew"
+          when :linux then "pow"
+          end
+        end
+
+        subject.stub(:translate, st) do
+          subject.push "hello"
+          subject.push "linux"
+
+          subject.each { |e| tested << "*#{e}*" }
+        end
+
+        tested.must_equal %w[*pew* *pow*]
+      end
+    end
+
+    describe "#to_s" do
+
+    end
+
+    describe "#pretty_print" do
+
+    end
   end
 end
